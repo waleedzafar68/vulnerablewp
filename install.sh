e204ff29e4b7cf956b02f8179317f117df144fb8
waleedzafar68 <91959220+waleedzafar68@users.noreply.github.com>
2022-08-30 07:15:11 +0500

    Manual install new Docker Version for Ubuntu

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 echo "Running apt update"
 sudo apt update
 echo "Installing docker-compose"
-sudo apt install docker-compose -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose #manual install
 echo "Running docker containers"
 docker-compose up -d
