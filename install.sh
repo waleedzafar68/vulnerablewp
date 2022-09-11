e199f2567c92df22b803fc7eba75d4909ea7b891
waleedzafar68 <91959220+waleedzafar68@users.noreply.github.com>
2022-09-11 23:16:22 +0500

    Fixed install.sh
    add -y to apt install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "Running apt update"
 sudo apt update
 echo "Installing docker and docker-compose"
-apt install docker.io
+apt install docker.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose #manual install
 echo "Running docker containers"
