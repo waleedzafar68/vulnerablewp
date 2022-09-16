b0efcde34f827de116732182757451aafae1f12d
waleedzafar68 <91959220+waleedzafar68@users.noreply.github.com>
2022-09-16 07:30:04 +0500

    Updated install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ snap install docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose #manual install
 echo "Running docker containers"
+docker version
 docker-compose up -d
