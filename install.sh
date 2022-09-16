91f6b322f665024952a148eebcc2796d24297948
waleedzafar68 <91959220+waleedzafar68@users.noreply.github.com>
2022-09-16 07:58:28 +0500

    Install.sh Manual install docker added

--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@
 echo "Running apt update"
 sudo apt update
 echo "Installing docker and docker-compose"
-snap install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose #manual install
 echo "Running docker containers"
-docker version
-service docker start
-service docker restart
 docker-compose up -d
